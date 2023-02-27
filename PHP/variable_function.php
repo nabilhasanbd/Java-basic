c31ba88ae7efb1810876fd0340738d01d5a05292
Nabil <nabil.uiu.bd@gmail.com>
2023-02-27 19:50:00 +0600

    Added variable function-2

--- PHP/variable_function.php
+++ PHP/variable_function.php
@@ -1,7 +1,18 @@
 <?php
-  function hello(){
-   echo "Hello Nabil hasan";
-  }
-  $var="Hello";
-  $var(); // prints "Hello Nabil hasan"
+
+function hello()
+{
+    echo "Hello Nabil hasan";
+}
+
+$var = "Hello";
+$var(); // prints "Hello Nabil hasan"
+
+function add($x, $y)
+{
+    echo $x + $y;
+}
+
+$var = "add";
+$var(10, 20); // 30
 ?>
