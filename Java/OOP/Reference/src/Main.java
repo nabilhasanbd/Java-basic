fa8e21b17a8e2b6c7b5b3ccba14a65a6ab6d0e36
Nabil <nabil.uiu.bd@gmail.com>
2023-03-03 12:13:07 +0600

    Added comment

--- Java/OOP/Reference/src/Main.java
+++ Java/OOP/Reference/src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         House blueHouse = new House("blue");
-        House anotherHouse = blueHouse; //
+        House anotherHouse = blueHouse; // Here no new object is created. anotherHouse is a referencing to the object blueHouse
 
-        System.out.println(blueHouse.getColor());
-        System.out.println(anotherHouse.getColor());
+        System.out.println(blueHouse.getColor()); //blue
+        System.out.println(anotherHouse.getColor()); //blue
 
         anotherHouse.setColor("Red");
-        System.out.println(anotherHouse.getColor());
-        System.out.println(blueHouse.getColor());
+        System.out.println(anotherHouse.getColor()); //red
+        System.out.println(blueHouse.getColor()); // red
 
         House greenHouse = new House("green");
         anotherHouse = greenHouse;
 
-        System.out.println(blueHouse.getColor());
-        System.out.println(anotherHouse.getColor());
-        System.out.println(greenHouse.getColor());
+        System.out.println(blueHouse.getColor()); //red
+        System.out.println(anotherHouse.getColor()); // green
+        System.out.println(greenHouse.getColor()); // green
 
     }
 }
