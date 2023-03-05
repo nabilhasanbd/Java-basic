63c6f87d45b3b36b14bbcf67e0ca6091e677d1fe
Nabil <nabil.uiu.bd@gmail.com>
2023-03-05 17:58:35 +0600

    Added set nextid

---
+++ SQL/set_next_id.sql
@@ -0,0 +1,12 @@
+-- Get Max ID from table
+SELECT MAX(id) FROM table;
+
+-- Get Next ID from table
+SELECT nextval('table_id_seq');
+
+-- Set Next ID Value to MAX ID
+SELECT setval('table_id_seq', (SELECT MAX(id) FROM table)+1);
+
+-- If any type error occurs
+
+SELECT setval('road_names_1_id_seq', (SELECT MAX(id)::bigint FROM road_names)+1);
