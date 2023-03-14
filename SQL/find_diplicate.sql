cf59447ed72ab7d9f94fbcda2885d80edbd5be12
Nabil <nabil.uiu.bd@gmail.com>
2023-03-14 13:03:49 +0600

    modified

--- SQL/find_diplicate.sql
+++ SQL/find_diplicate.sql
@@ -1,3 +1,9 @@
 select id, count(id) from road_names 
 group by id 
 having count(id) > 1;
+
+-- with number of  duplicates
+SELECT taxcd, COUNT(taxcd) as count
+FROM build_holdings
+GROUP BY taxcd
+HAVING COUNT(taxcd) > 1;
