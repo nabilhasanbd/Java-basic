e10ce35a57823353de8c0d92ececf7f348e0894e
Nabil <nabil.uiu.bd@gmail.com>
2023-03-15 13:05:04 +0600

    Added new query

--- SQL/set-sequence-value.sql
+++ SQL/set-sequence-value.sql
@@ -4,6 +4,11 @@ SELECT MAX(id) FROM table;
 -- Get Next ID from table
 SELECT nextval('table_id_seq');
 
+-- create sequence
+create sequence road_names_1_id_seq;
+alter table road_names
+    alter column id set default nextval('public.road_names_1_id_seq'::regclass);
+
 -- Set Next ID Value to MAX ID
 SELECT setval('table_id_seq', (SELECT MAX(id) FROM table)+1);
 
